c0443d722def11401ccf0ba9d5dee462231b0e31
matzal <mati.zalewski@gmail.com>
2018-05-07 21:29:20 +0200

    unit converter - initial commit

---
+++ src/main/java/jjdz4/mateusz/zalewski/UnitConverterImpl.java
@@ -0,0 +1,33 @@
+package jjdz4.mateusz.zalewski;
+
+public class UnitConverterImpl implements UnitConverter {
+    @Override
+    public double celsiusToFahrenheit(double c) {
+        return 0;
+    }
+
+    @Override
+    public double fahrenheitToCelsius(double f) {
+        return 0;
+    }
+
+    @Override
+    public double milesToKilometers(double m) {
+        return 0;
+    }
+
+    @Override
+    public double kilometersToMiles(double k) {
+        return 0;
+    }
+
+    @Override
+    public double poundsToKilograms(double p) {
+        return 0;
+    }
+
+    @Override
+    public double kilogramsToPounds(double k) {
+        return 0;
+    }
+}
