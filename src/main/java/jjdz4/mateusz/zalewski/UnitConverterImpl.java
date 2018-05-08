87bcc25ef791edf6e0b764396c7e4a3e78dd0751
matzal <mati.zalewski@gmail.com>
2018-05-08 20:20:58 +0200

    implement unit converter

--- src/main/java/jjdz4/mateusz/zalewski/UnitConverterImpl.java
+++ src/main/java/jjdz4/mateusz/zalewski/UnitConverterImpl.java
@@ -3,31 +3,31 @@ package jjdz4.mateusz.zalewski;
 public class UnitConverterImpl implements UnitConverter {
     @Override
     public double celsiusToFahrenheit(double c) {
-        return 0;
+        return (1.8*c + 32);
     }
 
     @Override
     public double fahrenheitToCelsius(double f) {
-        return 0;
+        return (f-32)*0.55555555555;
     }
 
     @Override
     public double milesToKilometers(double m) {
-        return 0;
+        return 1.60934*m;
     }
 
     @Override
     public double kilometersToMiles(double k) {
-        return 0;
+        return 0.621371*k;
     }
 
     @Override
     public double poundsToKilograms(double p) {
-        return 0;
+        return 0.453592*p;
     }
 
     @Override
     public double kilogramsToPounds(double k) {
-        return 0;
+        return 2.2046208*k;
     }
 }
